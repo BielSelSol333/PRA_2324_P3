6fdada694e84e59e4f69237da123c9b1370f99b7
BielSelSol <bielselsol@gmail.com>
2023-11-29 17:53:05 +0100

    Añadido clase virtual Dict.h

---
+++ Dict.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+template <typename V> class Dict{
+ public:
+  virtual void insert(std::string key, V value) = 0;
+  virtual V search(std::string key) = 0;
+  virtual V remove(std::string key) = 0;
+  virtual int entries() = 0;
+};
