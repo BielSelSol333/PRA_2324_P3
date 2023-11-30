df90f7c3930181c98fb8edb5d8777c783d86948d
BielSelSol <bielselsol@gmail.com>
2023-11-30 12:45:23 +0100

    Falta por acabar

--- HashTable.h
+++ HashTable.h
@@ -13,9 +13,51 @@ template <typename V> class HashTable : public Dict<V>{
 
   
  public:
-  HashTable(int size);
-  ~HashTable();
-  int capacity();
-  friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th);
-  V operator[](std::string key);
+  HashTable(int size){
+    table = new ListLinked<TableEntry<V>> [size];
+    n = 0;
+    max = size;
+  }
+  
+  ~HashTable(){
+    delete [] table;
+    n = 0;
+    max = 0;
+  }
+  
+  int capacity(){
+    return max;
+  }
+  
+  friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th){
+    out << "List => [";
+    for(int i=0; i<th.max; i++){
+      out << th.table->value << " ";
+    }
+    out << "]";
+    return out;
+  }
+  
+  V operator[](std::string key){
+    if(key != key){
+      throw std::runtime_error("No existe esa clave");
+    }
+    
+  }
+  
+  void insert(std::string key, V value)override{
+    
+  }
+  
+  V search(std::string key)override{
+    
+  }
+  
+  V remove(std::string key)override{
+
+  }
+  
+  int entries()override{
+    return n;
+  }
 };
